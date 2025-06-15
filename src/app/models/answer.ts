52bbc3778a49b2f1f30256e8dc1a193857e4e033
kaji-miki <kajimoto@dcl.cs.waseda.ac.jp>
2025-06-15 19:25:27 +0900

    アンケート項目の追加

---
+++ src/app/models/answer.ts
@@ -0,0 +1,22 @@
+type Answers = {
+  q1: string;
+  q2: string;
+  q3: string;
+  q4: string;
+  q5: string[]; // ← チェックボックス
+  q6: string;
+  q7: string[]; // ← チェックボックス
+  q8: string;
+  q9: string;
+  q10: string;
+  q11: string;
+  q12: string;
+  q13: string[]; // ← チェックボックス
+  q14: string;
+  q15: string;
+  q16: string;
+  q17: string;
+  q18: string;
+  q19: string;
+  q20: string;
+};
